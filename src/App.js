c2123835478b23f01cc0f5262e3e68d9632a3faf
Dada-pahane <dadasahebpahane@gmail.com>
2024-05-26 18:41:02 +0530

    inital commit for ecomm-dashbor-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, SplitButton} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jai Shree Gurudev datta</h1>
+      <h5>E - Commers Project</h5>
+      <button>Noram Button</button>
+      <Button>Boostarp Button</Button>
     </div>
   );
 }
